cfe18700ecbb37aa8e08295291c4766cf857a678
jaylee518 <jaylee.watcher@gmail.com>
2024-08-31 09:30:37 +0900

    fix:escrow-funded
    Co-authored-by: pixelcodecrafter <ultantaylor@gmail.com>
    Co-authored-by: jaylee518 <jaylee.watcher@gmail.com>

--- src/escrow.rs
+++ src/escrow.rs
@@ -8,22 +8,22 @@ mod escrow {
     use super::*;
 
     #[ink(storage)]
-    // pub struct Escrow {
-    //     pub buyer: AccountId,
-    //     pub seller: AccountId,
-    //     pub arbiter: AccountId,
-    //     pub amount: Balance,
-    //     pub is_funded: bool,
-    //     pub is_released: bool,
-    // }
+    pub struct Escrow {
+        pub buyer: AccountId,
+        pub seller: AccountId,
+        pub arbiter: AccountId,
+        pub amount: Balance,
+        pub is_funded: bool,
+        pub is_released: bool,
+    }
 
     #[ink(event)]
-    pub struct Funded {
-        #[ink(topic)]
-        buyer: AccountId,
-        #[ink(topic)]
-        amount: Balance,
-    }
+    // pub struct Funded {
+    //     #[ink(topic)]
+    //     buyer: AccountId,
+    //     #[ink(topic)]
+    //     amount: Balance,
+    // }
 
     #[ink(event)]
     pub struct Released {
